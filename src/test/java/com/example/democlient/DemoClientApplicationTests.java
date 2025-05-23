fd2f761f0a8ca6ade172c5b8627e6e511c2b509e
unknown <Duoc@ALAWSA04LC1630.duoc.acad>
2025-05-23 19:51:24 -0400

    Agregue

--- src/test/java/com/example/democlient/DemoClientApplicationTests.java
+++ src/test/java/com/example/democlient/DemoClientApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.democlient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//ejemplotest
 @SpringBootTest
 class DemoClientApplicationTests {
 
